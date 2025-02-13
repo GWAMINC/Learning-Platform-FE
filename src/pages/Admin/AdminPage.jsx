e22dc99c5aeec05cf0909ed7cd5e5090b58cc4e4
Admin <scclegion243@gmail.com>
2025-02-13 10:01:18 +0700

    fix bottom menu admin

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import { FaBars, FaUser, FaChalkboard, FaBook, FaPlus, FaEdit, FaTrash, FaEye } from "react-icons/fa";
+import { FaBars, FaUser, FaChalkboard, FaBook } from "react-icons/fa";
 import "./AdminPage.css";
-import {useNavigate, Outlet} from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
 
 const AdminPage = () => {
     const [isOpen, setIsOpen] = useState(true);
-    const [isCourseOpen, setIsCourseOpen] = useState(false);
     const navigate = useNavigate();
 
     return (
@@ -18,44 +17,30 @@ const AdminPage = () => {
                 </button>
 
                 {/* Sidebar Menu */}
-                <ul className="menu">
-                    {/* Course Section */}
-                    <li className="menu-item" onClick={() => navigate("/admin/courses")}>
-                        <div className="menu-title" >
+                <div className="menu">
+                    <div className="menu-item" onClick={() => navigate("/admin/courses")}>
+                        <div className="menu-title">
                             <FaBook />
                             {isOpen && <span>Course</span>}
                         </div>
-                    </li>
-                </ul>
-
+                    </div>
+                </div>
 
                 {/* Bottom Menu */}
-                <ul className="menu bottom-menu">
-                    <hr className="divider-top"/>
-                    <li className="menu-item">
-                        <FaChalkboard/> {isOpen && <span>Dashboard</span>}
-                    </li>
-                    <li className="menu-item">
-                        <FaUser/> {isOpen && <span>Admin Profile</span>}
-                    </li>
-                </ul>
+                <div className="bottom-menu">
+                    <hr className="divider-top" />
+                    <div className="menu-item">
+                        <FaChalkboard /> {isOpen && <span>Dashboard</span>}
+                    </div>
+                    <div className="menu-item">
+                        <FaUser /> {isOpen && <span>Admin Profile</span>}
+                    </div>
+                </div>
             </div>
-
-            {/* Dropdown Course (Nổi ngoài sidebar) */}
-            {/*{isCourseOpen && (*/}
-            {/*    <div className="submenu-container show">*/}
-            {/*        <ul className="submenu">*/}
-            {/*            <li><FaPlus /> <span>Create Course</span></li>*/}
-            {/*            <li><FaEye /> <span>View Courses</span></li>*/}
-            {/*            <li><FaEdit /> <span>Edit Course</span></li>*/}
-            {/*            <li><FaTrash /> <span>Delete Course</span></li>*/}
-            {/*        </ul>*/}
-            {/*    </div>*/}
-            {/*)}*/}
 
             {/* Content Area */}
             <div className="content">
-                <Outlet/>
+                <Outlet />
             </div>
         </div>
     );
